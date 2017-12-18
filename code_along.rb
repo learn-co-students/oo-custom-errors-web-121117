4a507877203c9443c1f70ea5a6609b959205db1c
github username <github email address>
2017-12-17 20:36:47 -0500

    Done.

---
+++ code_along.rb
@@ -0,0 +1,12 @@
+class Person
+  attr_accessor :name, :partner
+
+  def initialize(name)
+    @name = name
+  end
+
+  def get_married(person)
+    self.partner = person
+    person.partner = self
+  end
+end
